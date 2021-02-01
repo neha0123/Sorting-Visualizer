bd518bf1c059e34ea8bcf33b949c264d0ff12dcc
Neha Garg <garg22neha1999@gmail.com>
2021-02-01 14:49:06 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>neha garg</h1>
+      <h2>author:nikky</h2>
+    </div>
+  );
+}
+
+export default App;
