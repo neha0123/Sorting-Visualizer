a0e0d10ce303593cbe6357e0f7928ad4a6a5bb7f
Neha Garg <garg22neha1999@gmail.com>
2021-02-01 15:40:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
+import profileImage from './img/1.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>neha garg</h1>
-      <h2>author:nikky</h2>
+      <h1>HELLO WORLD</h1>
+     <h2>author:neha garg</h2>
+     <img src={profileImage} alt="profile-image"/>
+     
     </div>
   );
 }
